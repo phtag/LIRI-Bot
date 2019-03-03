7f2a687d3d57bc2d0ff3a4820c9cfdb5cb853287
Peter Tag <peter.h.tag@gmail.com>
2019-03-03 08:20:14 -0800

    Work in progress

--- liri.js
+++ liri.js
@@ -6,22 +6,51 @@ var Spotify = require('node-spotify-api');
 var UserRequest = process.argv[2];
 
 if (UserRequest === 'concert-this') {
-    var artist = process.argv[3];
+  // var artist = process.argv.slice(3).join('+');
+  var artist = process.argv.slice(3).join(' ');
+  console.log('Checking for artist=' + artist)
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     axios.get(queryURL).then(function(response) {
 
       // Printing the entire object to console
-      console.log(response);
+      // console.log(response.data);
+      console.log(response.data[0]);
+      console.log('================================');
+      response.data.forEach(function(element) {
+        var lineUp = "";
+        // Because there can be more than one band at the concert, 
+        // build a line up string by iterating over the linup array elements
+        element.lineup.forEach(function(element) {
+          if (lineUp === "") {
+            lineUp = element;
+          } else {
+            lineUp += ', ' + element;
+          }
+        });
+        console.log(lineUp);
+        console.log(element.venue.name);
+        console.log(element.venue.city + "," + element.venue.country);
+        console.log(element.datetime);
+        console.log('================================');
+//         Name of the venue
+// Venue location
+// Date of the Event (use moment to format this as "MM/DD/YYYY")
+      })
 
       // Constructing HTML containing the artist information
     });
 } else if (UserRequest === 'spotify-this-song') {
+  //________
+  // SPOTIFY
+  //
     var spotify = new Spotify({
       id: process.env.SPOTIFY_ID,
       secret: process.env.SPOTIFY_SECRET
     });    
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-      if (err) {
+    spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+      // spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+      // spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+          if (err) {
         return console.log('Error occurred: ' + err);
       }  
     //   data.tracks.items.artists.foreach(function(element) {
@@ -29,15 +58,23 @@ if (UserRequest === 'concert-this') {
 
     // console.log(data.tracks.items); 
     data.tracks.items.forEach(function(element) {
-        // console.log(element);
+        //console.log(element);
         // console.log(element.album);
         // console.log(element);
         // console.log(element.album.name);
         // console.log(element.album);
         // console.log(element.album);
-        // console.log(element);
-        // console.log(element.artists);
-        console.log(element.album);
+        var qArtists = element.album.artists;
+        qArtists.forEach(function(thisElement) {
+          console.log(thisElement.external_urls);
+        })
+
+        // GOOD Results...
+        // console.log(element.album.artists);
+        // console.log(element.album.id);
+        // console.log(element.album.name);
+        // console.log(element.album.release_date);
+        // console.log(element.album.external_urls);
         // console.log(element.album.total_tracks);
     })
     // console.log(data.tracks); 
