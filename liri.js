bde588f96a06057e521f4ad2518cc208b2da702f
Peter Tag <peter.h.tag@gmail.com>
2019-03-03 10:10:41 -0800

    Updates

--- liri.js
+++ liri.js
@@ -3,43 +3,42 @@ var keys = require("./keys.js");
 var axios = require("axios");
 var Spotify = require('node-spotify-api'); 
 var moment = require('moment');
-
+var fs = require('fs');
 var UserRequest = process.argv[2];
-
 if (UserRequest === 'concert-this') {
-  // var artist = process.argv.slice(3).join('+');
   var artist = process.argv.slice(3).join(' ');
   console.log('Checking for artist=' + artist)
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-    axios.get(queryURL).then(function(response) {
+  concertThis(artist);
+//     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+//     axios.get(queryURL).then(function(response) {
 
-      // Printing the entire object to console
-      // console.log(response.data);
-      console.log(response.data[0]);
-      console.log('================================');
-      response.data.forEach(function(element) {
-        var lineUp = "";
-        // Because there can be more than one band at the concert, 
-        // build a line up string by iterating over the linup array elements
-        element.lineup.forEach(function(element) {
-          if (lineUp === "") {
-            lineUp = element;
-          } else {
-            lineUp += ', ' + element;
-          }
-        });
-        console.log('Performing bands: '+ lineUp);
-        console.log('Venue name: ' + element.venue.name);
-        console.log('Venue location: ' + element.venue.city + ", " + element.venue.country);
-        console.log('Date: ' + moment(element.datetime).format('LLLL'));
-        console.log('================================');
-//         Name of the venue
-// Venue location
-// Date of the Event (use moment to format this as "MM/DD/YYYY")
-      })
+//       // Printing the entire object to console
+//       // console.log(response.data);
+//       console.log(response.data[0]);
+//       console.log('================================');
+//       response.data.forEach(function(element) {
+//         var lineUp = "";
+//         // Because there can be more than one band at the concert, 
+//         // build a line up string by iterating over the linup array elements
+//         element.lineup.forEach(function(element) {
+//           if (lineUp === "") {
+//             lineUp = element;
+//           } else {
+//             lineUp += ', ' + element;
+//           }
+//         });
+//         console.log('Performing bands: '+ lineUp);
+//         console.log('Venue name: ' + element.venue.name);
+//         console.log('Venue location: ' + element.venue.city + ", " + element.venue.country);
+//         console.log('Date: ' + moment(element.datetime).format('LLLL'));
+//         console.log('================================');
+// //         Name of the venue
+// // Venue location
+// // Date of the Event (use moment to format this as "MM/DD/YYYY")
+//       })
 
-      // Constructing HTML containing the artist information
-    });
+//       // Constructing HTML containing the artist information
+//     });
 } else if (UserRequest === 'spotify-this-song') {
   //________
   // SPOTIFY
@@ -82,6 +81,9 @@ if (UserRequest === 'concert-this') {
     });
 } else if (UserRequest === 'movie-this') {
   var movie = process.argv.slice(3).join('+');
+  if (movie === '') {
+    movie = 'Mr. Nobody';
+  }
 
   // axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
   axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
@@ -105,4 +107,36 @@ if (UserRequest === 'concert-this') {
     }
       );
 } else if (UserRequest === 'do-what-it-says') {
+  fs.readFile('./random.txt', 'utf8', function(error, data) {
+    var arguments = data.split(',');
+  console.log(arguments);
+  });
+}
+//_______________________________________________________________________________
+//  FUNCTIONS
+//
+function concertThis(artist) {
+  // Build a query for BandsinTown to return a list of all events that the specified artist
+  // will be performing in soon
+  var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  axios.get(queryURL).then(function(response) {
+    console.log('================================');
+    response.data.forEach(function(element) {
+      var lineUp = "";
+      // Because there can be more than one band at the concert, 
+      // build a line up string by iterating over the linup array elements
+      element.lineup.forEach(function(element) {
+        if (lineUp === "") {
+          lineUp = element;
+        } else {
+          lineUp += ', ' + element;
+        }
+      });
+      console.log('Performing bands: '+ lineUp);
+      console.log('Venue name: ' + element.venue.name);
+      console.log('Venue location: ' + element.venue.city + ", " + element.venue.country);
+      console.log('Date: ' + moment(element.datetime).format('LLLL'));
+      console.log('================================');
+    })
+  });
 }
