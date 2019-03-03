8cf2164d41799976911a529cc09d9a8a7f36bd7f
Peter Tag <peter.h.tag@gmail.com>
2019-03-03 13:03:44 -0800

    Updates...

--- liri.js
+++ liri.js
@@ -5,97 +5,46 @@ var Spotify = require('node-spotify-api');
 var moment = require('moment');
 var fs = require('fs');
 var UserRequest = process.argv[2];
-if (UserRequest === 'concert-this') {
-  var artist = process.argv.slice(3).join(' ');
-  console.log('Checking for artist=' + artist)
-  concertThis(artist);
-//     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-//     axios.get(queryURL).then(function(response) {
+respondToUserRequest(UserRequest, process.argv.slice(3));
 
-//       // Printing the entire object to console
-//       // console.log(response.data);
-//       console.log(response.data[0]);
-//       console.log('================================');
-//       response.data.forEach(function(element) {
-//         var lineUp = "";
-//         // Because there can be more than one band at the concert, 
-//         // build a line up string by iterating over the linup array elements
-//         element.lineup.forEach(function(element) {
-//           if (lineUp === "") {
-//             lineUp = element;
-//           } else {
-//             lineUp += ', ' + element;
-//           }
-//         });
-//         console.log('Performing bands: '+ lineUp);
-//         console.log('Venue name: ' + element.venue.name);
-//         console.log('Venue location: ' + element.venue.city + ", " + element.venue.country);
-//         console.log('Date: ' + moment(element.datetime).format('LLLL'));
-//         console.log('================================');
-// //         Name of the venue
-// // Venue location
-// // Date of the Event (use moment to format this as "MM/DD/YYYY")
-//       })
-
-//       // Constructing HTML containing the artist information
-//     });
-} else if (UserRequest === 'spotify-this-song') {
-  //________
-  // SPOTIFY
-  //
-    var spotify = new Spotify({
-      id: process.env.SPOTIFY_ID,
-      secret: process.env.SPOTIFY_SECRET
-    });    
-    spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
-      // spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
-      // spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
-          if (err) {
-        return console.log('Error occurred: ' + err);
-      }  
-    //   data.tracks.items.artists.foreach(function(element) {
-    //       console.log(element);
-
-    // console.log(data.tracks.items); 
-    data.tracks.items.forEach(function(element) {
-        //console.log(element);
-        // console.log(element.album);
-        // console.log(element);
-        // console.log(element.album.name);
-        // console.log(element.album);
-        // console.log(element.album);
-        var qArtists = element.album.artists;
-        qArtists.forEach(function(thisElement) {
-          console.log(thisElement.external_urls);
-        })
-
-        // GOOD Results...
-        // console.log(element.album.artists);
-        // console.log(element.album.id);
-        // console.log(element.album.name);
-        // console.log(element.album.release_date);
-        // console.log(element.album.external_urls);
-        // console.log(element.album.total_tracks);
-    })
-    // console.log(data.tracks); 
-    });
-} else if (UserRequest === 'movie-this') {
-  var movie = process.argv.slice(3).join('+');
-  if (movie === '') {
-    movie = 'Mr. Nobody';
-  }
-  movieThis(movie);
-  // axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-
-} else if (UserRequest === 'do-what-it-says') {
-  fs.readFile('./random.txt', 'utf8', function(error, data) {
-    var arguments = data.split(',');
-  console.log(arguments);
-  });
-}
 //_______________________________________________________________________________
 //  FUNCTIONS
 //
+function respondToUserRequest(UserRequest, queryItem) {
+  if (UserRequest === 'concert-this') {
+    // var artist = process.argv.slice(3).join(' ');
+    var artist = queryItem.join(' ');
+    console.log('Checking for artist=' + artist)
+    concertThis(artist);
+  } else if (UserRequest === 'spotify-this-song') {
+    //________
+    // SPOTIFY
+    //
+    // var song = process.argv.slice(3).join(' ');
+    var song = queryItem.join(' ');
+
+    spotifyThisSong(song);
+
+  } else if (UserRequest === 'movie-this') {
+    // var movie = process.argv.slice(3).join('+');
+    var movie = queryItem.join('+');
+    if (movie === '') {
+      movie = 'Mr. Nobody';
+    }
+    movieThis(movie);
+    // axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+
+  } else if (UserRequest === 'do-what-it-says') {
+    fs.readFile('./random.txt', 'utf8', function(error, data) {
+      var arguments = data.split(',');
+      console.log(arguments);
+      var which = arguments[0];
+      var queryItem = arguments[1].split(' ');
+      respondToUserRequest(arguments[0], queryItem);
+    });
+  }
+}
+
 function concertThis(artist) {
   // Build a query for BandsinTown to return a list of all events that the specified artist
   // will be performing in soon
@@ -119,6 +68,45 @@ function concertThis(artist) {
       console.log('Date: ' + moment(element.datetime).format('LLLL'));
       console.log('================================');
     })
+  });
+}
+
+function spotifyThisSong(song) {
+  var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  });    
+  spotify.search({ type: 'track', query: song }, function(err, data) {
+    // spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+    // spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+        if (err) {
+      return console.log('Error occurred: ' + err);
+    }  
+  //   data.tracks.items.artists.foreach(function(element) {
+  //       console.log(element);
+
+  // console.log(data.tracks.items); 
+  data.tracks.items.forEach(function(element) {
+      //console.log(element);
+      // console.log(element.album);
+      // console.log(element);
+      // console.log(element.album.name);
+      // console.log(element.album);
+      // console.log(element.album);
+      var qArtists = element.album.artists;
+      qArtists.forEach(function(thisElement) {
+        console.log(thisElement.external_urls);
+      })
+
+      // GOOD Results...
+      // console.log(element.album.artists);
+      // console.log(element.album.id);
+      // console.log(element.album.name);
+      // console.log(element.album.release_date);
+      // console.log(element.album.external_urls);
+      // console.log(element.album.total_tracks);
+  })
+  // console.log(data.tracks); 
   });
 }
 
